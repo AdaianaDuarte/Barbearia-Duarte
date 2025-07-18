873e08e657d71a165013faadcd71274d9f7c52cd
AdaianaDuarte <daianaduarte0083@gmail.com>
2025-07-18 20:35:29 -0300

    Este é um site de página única.
    É um site de uma barbearia que conta com seções de home, sobre, serviços, galeria e formulário de contato.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Rolagem suave para os links do menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
